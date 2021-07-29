3b0f8b68bcca1dd8e6d81a5db00659fb9000c009
Emmanouil Karystinaios <emmanouil.karystinaios@jku.at>
2021-07-29 13:45:52 +0300

    added homogeneous graph data

--- scripts/nc_dataset_class.py
+++ scripts/nc_dataset_class.py
@@ -28,13 +28,27 @@ FILE_LIST = [
 	'note.csv', 'rest-follows-note.csv', 'rest.csv'	
 	]
 
+def min_max_scaler(X):
+	data_min = np.nanmin(X, axis=0)
+    data_max = np.nanmax(X, axis=0)
+	data_range = data_max - data_min
+   	X_std = (X - data_min) / data_range
+    return X_std
+
 class MozartPianoGraphDataset(DGLDataset):
-	def __init__(self, url, raw_dir=None, add_inverse_edges=False, add_aug=True, select_piece=None):
+	def __init__(self, name, url, raw_dir=None, add_inverse_edges=False, add_aug=True, select_piece=None, features=None, normalize=False):
+		if features:
+			self.note_features = features
+			self.rest_features = features.remove("pitch") if "pitch" in features else features 
+		else :
+			self.note_features = ["onset", "duration", "ts", "pitch"]
+			self.rest_features = ["onset", "duration", "ts"]
+		self.normalize = normalize
 		self.add_inverse_edges = add_inverse_edges
 		self.add_aug = add_aug
 		self.select_piece = select_piece
 		# url = "https://raw.githubusercontent.com/melkisedeath/tonnetzcad/main/node_classification/mozart_piano_sonatas/"
-		super().__init__(name='mozart_piano_sonatas_graph', raw_dir=raw_dir, url=url)
+		super().__init__(name=name, raw_dir=raw_dir, url=url)
 
 	def process(self):
 		self.PIECE_LIST = PIECE_LIST
@@ -45,11 +59,11 @@ class MozartPianoGraphDataset(DGLDataset):
 				path = self.url + "/" + self.select_piece + "/" + csv
 				if csv == "note.csv":
 					notes = pd.read_csv(path)
-					note_node_features = torch.from_numpy(notes[["onset", "duration", "pitch"]].to_numpy())
+					note_node_features = torch.from_numpy(notes[self.note_features].to_numpy())
 					note_node_labels = torch.from_numpy(notes['label'].astype('category').cat.codes.to_numpy()).long()
 				elif csv == "rest.csv":      
 					rests = pd.read_csv(path)
-					a = rests[["onset", "duration"]].to_numpy()
+					a = rests[self.rest_features].to_numpy()
 					rest_node_features = torch.from_numpy(np.hstack((a,np.zeros((a.shape[0],1)))))
 					rest_node_labels = torch.from_numpy(rests['label'].astype('category').cat.codes.to_numpy()).long()
 				else :
@@ -71,120 +85,80 @@ class MozartPianoGraphDataset(DGLDataset):
 			self.graph.nodes['note'].data['labels'] = note_node_labels
 			self.graph.nodes['rest'].data['feature'] = rest_node_features.float()
 			self.graph.nodes['rest'].data['labels'] = rest_node_labels
-		else:
-			if 'mozart' in self.raw_dir : 
-				print(self.raw_dir)    
-				if all([os.path.isdir(os.path.join(self.raw_dir, fn)) for fn in os.listdir(self.raw_dir)]):
-					for fn in os.listdir(self.raw_dir):
-						print(fn)
-						edge_dict = dict()
-						for csv in os.listdir(os.path.join(self.raw_dir, fn)):
-							if csv == "note.csv":
-								notes = pd.read_csv(os.path.join(self.raw_dir, fn, "note.csv"))
-								note_node_features = torch.from_numpy(notes[["onset", "duration", "pitch"]].to_numpy())
-								note_node_labels = torch.from_numpy(notes['label'].astype('category').cat.codes.to_numpy()).long()
+		else:			  
+			for fn in self.PIECE_LIST:
+				print(fn)
+				edge_dict = dict()
+				for csv in self.FILE_LIST:
+					path = self.url + "/" + fn + "/" + csv
+					if csv == "note.csv":
+						notes = pd.read_csv(path)
+						a = notes[self.note_features].to_numpy()
+						if self.normalize:
+							a = min_max_scaler(a)
+						note_node_features = torch.from_numpy(a)
+						note_node_labels = torch.from_numpy(notes['label'].astype('category').cat.codes.to_numpy()).long()
+					elif csv == "rest.csv":      
+						rests = pd.read_csv(path)
+						a = rests[self.rest_features].to_numpy()
+						if self.normalize:
+							a = min_max_scaler(a)
+						if len(self.note_features) > len(self.rest_features):
+							rest_node_features = torch.from_numpy(np.hstack((a,np.zeros((a.shape[0],1)))))
+						else : 
+							rest_node_features = torch.from_numpy(a)
+						rest_node_labels = torch.from_numpy(rests['label'].astype('category').cat.codes.to_numpy()).long()
+					# elif csv == "ts.csv":
+					# 	ts = pd.read_csv(path)
+					# 	rest_node_features = torch.from_numpy(np.zeros((ts.shape[0],3)))
+					# 	rest_node_labels = torch.from_numpy(rests['label'].astype('category').cat.codes.to_numpy()).long()
+					else :
+						name = tuple(csv.split(".")[0].split("-"))
+						edges_data = pd.read_csv(path)
+						if edges_data.empty:   
+							edges_src = torch.tensor([0])
+							edges_dst = torch.tensor([0])       				
+						else :
+							edges_src = torch.from_numpy(edges_data['src'].to_numpy())
+							edges_dst = torch.from_numpy(edges_data['des'].to_numpy())
+						edge_dict[name] = (edges_src, edges_dst)
+						if self.add_inverse_edges:
+							inv_name = (name[2], name[1]+"_inv", name[0])
+							edge_dict[inv_name] = (edges_dst, edges_src)
 
-							elif csv == "rest.csv":      
-								rests = pd.read_csv(os.path.join(self.raw_dir, fn, "rest.csv"))
-								a = rests[["onset", "duration"]].to_numpy()
-								rest_node_features = torch.from_numpy(np.hstack((a,np.zeros((a.shape[0],1)))))
-								rest_node_labels = torch.from_numpy(rests['label'].astype('category').cat.codes.to_numpy()).long()
-							else :
-								name = tuple(csv.split(".")[0].split("-"))
-								edges_data = pd.read_csv(os.path.join(self.raw_dir, fn, csv)) 
-								if edges_data.empty:   
-									edges_src = torch.tensor([0])
-									edges_dst = torch.tensor([0])       				
-								else :
-									edges_src = torch.from_numpy(edges_data['src'].to_numpy())
-									edges_dst = torch.from_numpy(edges_data['des'].to_numpy())
-								edge_dict[name] = (edges_src, edges_dst)
-								if self.add_inverse_edges:
-									inv_name = (name[2], name[1]+"_inv", name[0])
-									edge_dict[inv_name] = (edges_dst, edges_src)
+
+				try:
+					g = self.graph
+					
+					graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" :rests.shape[0]})
+					graph.nodes['note'].data['feature'] = note_node_features.float()
+					graph.nodes['note'].data['labels'] = note_node_labels
+					graph.nodes['rest'].data['feature'] = rest_node_features.float()
+					graph.nodes['rest'].data['labels'] = rest_node_labels
+					self.graph = dgl.batch_hetero([g, graph])
+				except AttributeError:
+					self.graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" : rests.shape[0]})
+					self.graph.nodes['note'].data['feature'] = note_node_features.float()
+					self.graph.nodes['note'].data['labels'] = note_node_labels
+					self.graph.nodes['rest'].data['feature'] = rest_node_features.float()
+					self.graph.nodes['rest'].data['labels'] = rest_node_labels
 
 
-						try:
-							g = self.graph
+				# Perform Data Augmentation
+				if self.add_aug:
+					for _ in range(5):
+						g = self.graph
+						resize_factor = random.choice([0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5])
+						n = random.choice(range(-6, 6, 1))
+						if resize_factor != 1 or n != 0:
+							dur_resize = torch.tensor([resize_factor, resize_factor, resize_factor, 1]).float()
+							pitch_aug = torch.tensor([0, 0, 0, n]).float()
 							graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" :rests.shape[0]})
-							graph.nodes['note'].data['feature'] = note_node_features.float()
+							graph.nodes['note'].data['feature'] = note_node_features.float()*dur_resize + pitch_aug
 							graph.nodes['note'].data['labels'] = note_node_labels
 							graph.nodes['rest'].data['feature'] = rest_node_features.float()
 							graph.nodes['rest'].data['labels'] = rest_node_labels
 							self.graph = dgl.batch_hetero([g, graph])
-						except AttributeError:
-							self.graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" : rests.shape[0]})
-							self.graph.nodes['note'].data['feature'] = note_node_features.float()
-							self.graph.nodes['note'].data['labels'] = note_node_labels
-							self.graph.nodes['rest'].data['feature'] = rest_node_features.float()
-							self.graph.nodes['rest'].data['labels'] = rest_node_labels
-			else:    
-
-				for fn in self.PIECE_LIST:
-					print(fn)
-					edge_dict = dict()
-					for csv in self.FILE_LIST:
-						path = self.url + "/" + fn + "/" + csv
-						if csv == "note.csv":
-							notes = pd.read_csv(path)
-							note_node_features = torch.from_numpy(notes[["onset", "duration", "pitch"]].to_numpy())
-							note_node_labels = torch.from_numpy(notes['label'].astype('category').cat.codes.to_numpy()).long()
-						elif csv == "rest.csv":      
-							rests = pd.read_csv(path)
-							a = rests[["onset", "duration"]].to_numpy()
-							rest_node_features = torch.from_numpy(np.hstack((a,np.zeros((a.shape[0],1)))))
-							rest_node_labels = torch.from_numpy(rests['label'].astype('category').cat.codes.to_numpy()).long()
-						elif csv == "ts.csv":
-							ts = pd.read_csv(path)
-							rest_node_features = torch.from_numpy(np.zeros((ts.shape[0],3)))
-							rest_node_labels = torch.from_numpy(rests['label'].astype('category').cat.codes.to_numpy()).long()
-						else :
-							name = tuple(csv.split(".")[0].split("-"))
-							edges_data = pd.read_csv(path)
-							if edges_data.empty:   
-								edges_src = torch.tensor([0])
-								edges_dst = torch.tensor([0])       				
-							else :
-								edges_src = torch.from_numpy(edges_data['src'].to_numpy())
-								edges_dst = torch.from_numpy(edges_data['des'].to_numpy())
-							edge_dict[name] = (edges_src, edges_dst)
-							if self.add_inverse_edges:
-								inv_name = (name[2], name[1]+"_inv", name[0])
-								edge_dict[inv_name] = (edges_dst, edges_src)
-
-
-					try:
-						g = self.graph
-						
-						graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" :rests.shape[0]})
-						graph.nodes['note'].data['feature'] = note_node_features.float()
-						graph.nodes['note'].data['labels'] = note_node_labels
-						graph.nodes['rest'].data['feature'] = rest_node_features.float()
-						graph.nodes['rest'].data['labels'] = rest_node_labels
-						self.graph = dgl.batch_hetero([g, graph])
-					except AttributeError:
-						self.graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" : rests.shape[0]})
-						self.graph.nodes['note'].data['feature'] = note_node_features.float() + torch.tensor([0, 0 , -6]).float()
-						self.graph.nodes['note'].data['labels'] = note_node_labels
-						self.graph.nodes['rest'].data['feature'] = rest_node_features.float()
-						self.graph.nodes['rest'].data['labels'] = rest_node_labels
-
-
-					# Perform Data Augmentation
-					if self.add_aug:
-						for _ in range(5):
-							g = self.graph
-							resize_factor = random.choice([0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5])
-							n = random.choice(range(-6, 6, 1))
-							if resize_factor != 1 or n != 0:
-								dur_resize = torch.tensor([resize_factor, resize_factor, 1]).float()
-								pitch_aug = torch.tensor([0, 0 , n]).float()
-								graph = dgl.heterograph(edge_dict, num_nodes_dict={"note" : notes.shape[0], "rest" :rests.shape[0]})
-								graph.nodes['note'].data['feature'] = note_node_features.float()*dur_resize + pitch_aug
-								graph.nodes['note'].data['labels'] = note_node_labels
-								graph.nodes['rest'].data['feature'] = rest_node_features.float()
-								graph.nodes['rest'].data['labels'] = rest_node_labels
-								self.graph = dgl.batch_hetero([g, graph])
 
 
 		# If your dataset is a node classification dataset, you will need to assign
@@ -220,16 +194,20 @@ class MozartPianoGraphDataset(DGLDataset):
 
 class MPGD_cad(MozartPianoGraphDataset):
 	def __init__(self, raw_dir=None, add_inverse_edges=False, add_aug=True, select_piece=None):
-		url = "https://raw.githubusercontent.com/melkisedeath/tonnetzcad/main/node_classification/mozart_piano_sonatas/"
-		super.__init__(name='mpgd_cad', url=url)
+		url = "https://raw.githubusercontent.com/melkisedeath/tonnetzcad/main/node_classification/mps_ts_att_cadlab"
+		super().__init__(name='mpgd_cad', url=url)
 
 
 class MPGD_onset(MozartPianoGraphDataset):
 	def __init__(self, raw_dir=None, add_inverse_edges=False, add_aug=True, select_piece=None):
-		url = "https://raw.githubusercontent.com/melkisedeath/tonnetzcad/main/node_classification/mozart_piano_sonatas_onset/"
-		super.__init__(name='mpgd_onset', url=url)
+		url = "https://raw.githubusercontent.com/melkisedeath/tonnetzcad/main/node_classification/mps_ts_att_onlab/"
+		super().__init__(name='mpgd_onset', url=url)
 
-
+class MPGD_onset_test(MozartPianoGraphDataset)
+	def __init__(self, raw_dir=None, add_inverse_edges=False, add_aug=True, select_piece=None):
+		url = "https://raw.githubusercontent.com/melkisedeath/tonnetzcad/main/node_classification/mps_ts_att_onlab/"
+		super().__init__(name='mpgd_onset', url=url, raw_dir=raw_dir, 
+				add_inverse_edges=add_inverse_edges, add_aug=add_aug, select_piece=select_piece, features = ["onset", "ts"], normalize=True)
 
 
 if __name__ == "__main__":
